1b7a48b20d6d64c955bf327b3d6bc1fc67296e89
Leandro Acquati <leandroacquati@gmail.com>
2021-01-11 04:56:46 -0300

    config jest

---
+++ __tests__/user.test.ts
@@ -0,0 +1,19 @@
+import connection from './connection'
+
+describe('users', () => {
+  // beforeAll(async () => {
+  //   await connection.create()
+  // })
+
+  // afterAll(async () => {
+  //   await connection.close()
+  // })
+
+  // beforeEach(async () => {
+  //   await connection.clear()
+  // })
+
+  it('creates a user', () => {
+    expect(0).toBe(0)
+  })
+})
