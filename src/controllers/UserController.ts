ce4bea464e1549cee76fdb3796c1ed92d76f3879
Leandro Acquati <leandroacquati@gmail.com>
2021-01-19 10:07:49 -0300

    refactor

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -3,44 +3,42 @@ import { getRepository } from 'typeorm'
 import { validate } from 'class-validator'
 import User from '../entity/User'
 
-class UserController {
+export default class UserController {
   static listAll = async (request: Request, response: Response) => {
-    // Get users from database
     const userRepository = getRepository(User)
-    const users = await userRepository.find({
-      // We dont want to send the passwords on response
-      select: ['id', 'email', 'username', 'createDate', 'updateDate', 'role']
-    })
 
-    // Send the users object
-    return response.send(users)
+    try {
+      const users = await userRepository.find({
+        select: ['id', 'email', 'username', 'role', 'createDate', 'updateDate']
+      })
+      return response.send(users)
+    } catch (error) {
+      return response.status(404).json({ message: 'Users not found. ' + error })
+    }
   }
 
   static getOneById = async (request: Request, response: Response) => {
-    // Get the ID from the url
+    const userRepository = getRepository(User)
     const id = request.params.id
 
-    // Get the user from database
-    const userRepository = getRepository(User)
     try {
       const user = await userRepository.findOneOrFail(
         id,
         {
-          // We dont want to send the password on response
-          select: ['id', 'email', 'username', 'createDate', 'updateDate', 'role']
+          select: ['id', 'email', 'username', 'role', 'createDate', 'updateDate']
         }
       )
       return response.send(user)
     } catch (error) {
-      return response.status(404).send('User not found. ' + error)
+      return response.status(404).json({ message: 'User not found. ' + error })
     }
   }
 
   static newUser = async (request: Request, response: Response) => {
-    // Get parameters from the body
-    let { email, username, password, role } = request.body
+    const userRepository = getRepository(User)
+    const { email, username, password, role } = request.body
     let user = new User()
-    let date = new Date()
+    const date = new Date()
     user.email = email
     user.username = username
     user.password = password
@@ -48,81 +46,70 @@ class UserController {
     user.createDate = date
     user.updateDate = date
 
-    // Validade if the parameters are ok
     const errors = await validate(user)
     if (errors.length > 0) {
-      return response.status(400).send(errors)
+      return response.status(400).json(errors)
     }
 
-    // Hash the password, to securely store on DB
     user.hashPassword()
 
-    // Try to save. If fails, the email is already in use
-    const userRepository = getRepository(User)
     try {
       await userRepository.save(user)
     } catch (error) {
-      return response.status(409).send('Email or username already in use. ' + error)
+      return response.status(409).json({ message: 'Email or username already in use. ' + error })
     }
 
-    // If all ok, send 201 response
-    return response.status(201).send('User created successfully.')
+    return response.status(201).json({ message: 'User created successfully.' })
   }
 
   static editUser = async (request: Request, response: Response) => {
-    // Get the ID from the url
-    const id = request.params.id
-
-    // Get values from the body
-    const { email, username, role } = request.body
-
-    // Try to find user on database
     const userRepository = getRepository(User)
+    const id = request.params.id
+    const { email, username, role } = request.body
     let user: User
+
     try {
       user = await userRepository.findOneOrFail(id)
     } catch (error) {
-      // If not found, send a 404 response
-      return response.status(404).send('User not found. ' + error)
+      return response.status(404).json({ message: 'User not found. ' + error })
     }
 
-    // Validate the new values on model
     user.email = email
     user.username = username
     user.role = role
     user.updateDate = new Date()
+
     const errors = await validate(user)
     if (errors.length > 0) {
-      return response.status(400).send(errors)
+      return response.status(400).json(errors)
     }
 
-    // Try to save. If fails, the email is already in use
     try {
       await userRepository.save(user)
     } catch (error) {
-      return response.status(409).send('Email or username already in use. ' + error)
+      return response.status(409).json({ message: 'Email or username already in use. ' + error })
     }
 
-    // After all send a 200 (OK) response
-    return response.status(200).send('User edited successfully.')
+    return response.status(200).json({ message: 'User edited successfully.' })
   }
 
   static deleteUser = async (request: Request, response: Response) => {
-    // Get the ID from the url
-    const id = request.params.id
-
     const userRepository = getRepository(User)
+    const id = request.params.id
     let user: User
+
     try {
       user = await userRepository.findOneOrFail(id)
     } catch (error) {
-      return response.status(404).send('User not found. ' + error)
+      return response.status(404).json({ message: 'User not found. ' + error })
     }
-    userRepository.delete(id)
 
-    // After all send a 200 (OK) response
-    return response.status(200).send('User successfully deleted.')
+    try {
+      await userRepository.delete(id)
+    } catch (error) {
+      return response.status(400).json({ message: + error })
+    }
+
+    return response.status(200).json({ message: 'User successfully deleted.' })
   }
 }
-
-export default UserController
---
+++ src/controllers/__tests__/listAll.test.ts
@@ -0,0 +1,35 @@
+import connection from '../../utils/connection'
+import supertest from 'supertest'
+import { app } from '../../app'
+import config from '../../config/config'
+
+describe('List All Users Test Suite', () => {
+  beforeAll(async () => {
+    await connection.create()
+  })
+
+  afterAll(async () => {
+    await connection.clear()
+    await connection.close()
+  })
+
+  beforeEach(async () => {
+    await connection.clear()
+    await connection.createTestAdmin()
+  })
+
+  test('List all users.', async (done) => {
+    const user = await supertest(app)
+      .post('/auth/login')
+      .send({ email: config.adminEmail, password: config.adminPassword })
+    expect(user.status).toBe(200)
+
+    const response = await supertest(app)
+      .patch('/auth/change-password')
+      .set({ token: user.body.token })
+      .send()
+    expect(response.status).toBe(400)
+
+    done()
+  })
+})
