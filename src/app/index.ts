da7ece33428bd4712a5cb93907aac9b04a116b5f
Leandro Acquati <leandroacquati@gmail.com>
2021-03-12 02:54:22 -0300

    remove body-parser

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,7 +1,6 @@
 import express from 'express'
 import cors from 'cors'
 import helmet from 'helmet'
-import bodyParser from 'body-parser'
 import routes from '../routes'
 
 // Create a new express application instance
@@ -10,7 +9,6 @@ const app = express()
 // Call midlewares
 app.use(cors())
 app.use(helmet())
-app.use(bodyParser.json())
 
 //Set all routes from routes folder
 app.use('/', routes)
